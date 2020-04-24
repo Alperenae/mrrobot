68357af4a8ee76607c2fd7d98556d4dcdd017e54
Alperen <alperen.ergel@yandex.com>
2020-04-24 22:37:09 +0300

    Add files via upload

---
+++ mrrobot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sleep 2 
+echo "root@Elliot:~# more readme.txt"
+sleep 1
+echo "-----readme.txt-----"
+echo "HELLO FRIEND "
+echo "--------------------"
+
+
+
+echo "Hello friend? That's lame."
+sleep 1
+echo "Maybe I should give you a name. But that's a slippery slope, you're only in my head, we have to remember that."
+sleep 1 
+echo "Shit, this actually happened, I'm talking to an imaginary person. What I'm about to tell you is top secret." 
+sleep 1
+echo "A conspiracy bigger than all of us. There's a powerful group of people out there that are secretly running the world." 
+sleep 1
+echo "I'm talking about the guys no one knows about, the ones that are invisible." 
+sleep 1
+echo "The top 1% of the top 1%, the guys that play God without permission. And now I think they're following me."
